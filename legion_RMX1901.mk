d3750c5a68ec688f2bde3f59d345e6a3c052c9bf
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-06-12 12:06:48 +0530

    Update and rename cherish_RMX1901.mk to legion_RMX1901.mk

--- legion_RMX1901.mk
+++ legion_RMX1901.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/legion/config/common_full_phone.mk)
+$(call inherit-product, vendor/legion/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
