535bf4954bb2b727e8840a9a930c16771b74fd72
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-06-15 17:07:56 +0530

    Alphadroid

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -9,14 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
-DEVICE_MAINTAINER := SHRAVAN
-SUPERIOR_UDFPS_ANIMATIONS := true
+ALPHA_BUILD_TYPE := UNOFFICIAL
+ALPHA_MAINTAINER := Shravansayzz..
 TARGET_ENABLE_BLUR := true
 
 # Inherit from RMX1901 device
@@ -25,7 +25,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := arrow_RMX1901
+PRODUCT_NAME := lineage_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
